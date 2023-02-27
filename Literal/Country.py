ff12163b1196369f05c19abcf9a8c7a3400db2c3
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-02-27 14:58:39 +0200

    Add files via upload

---
+++ Literal/Country.py
@@ -0,0 +1,25 @@
+﻿from module1 import *
+while True:
+    v=int(input("1-Otsige pealinna\n2-Leidke riik\n3-Lisa\n4-Parandamine\n5-Test"))
+    if v==1:
+        laused=Loe_fail("TextFile1.txt")
+        x=keyRiik(laused)
+        print(x)
+        print(laused)
+    elif v==2:
+        laused=reLoe_fail("TextFile1.txt")
+        x=keyPealiin(laused)
+        print(x)
+        print(laused)
+    elif v==3:
+        laused=Loe_fail("TextFile1.txt")
+        x=lisa_key(laused)
+        print(laused)
+    elif v==4:
+        laused=Loe_fail("TextFile1.txt")
+        x=parandamine(laused)
+        print(x)
+    elif v==5:
+        laused=Loe_fail("TextFile1.txt")
+        x=test(laused)
+        print(x)
---
+++ Literal/module1.py
@@ -0,0 +1,44 @@
+﻿from random import *
+def Loe_fail(f):
+    riigid=[]
+    sonastik = {}
+    file=open(f, "r", encoding="utf-8-sig")
+    for line in file:
+        k, v=line.split("-")
+        sonastik[k] = v
+        riigid.append(k)
+    file.close()
+    return sonastik
+def reLoe_fail(f):
+    sonastik = {}
+    file=open(f, "r", encoding="utf-8-sig")
+    for line in file:
+        k, v=line.split("-")
+        sonastik[v] = k
+    file.close()
+    return sonastik
+def keyRiik(f:dict):
+    xc=input("Sise riik: ")
+    x=f.get(xc)
+    return x
+def keyPealiin(f:dict):
+    xc=input("Sise pealiin: ")
+    x=f.get(xc)
+    return x
+def lisa_key(f):
+    xc=input("Sisse riik: ")
+    cx=input("Sisse pealinn: ")
+    f.update({xc:cx})
+    return f
+def parandamine(f:dict):
+    ds=input("Kirjutage riik, mida parandada: ")
+    f.pop(ds)
+    xc=input("Sisse riik: ")
+    cx=input("Sisse pealinn: ")
+    f.update({xc:cx})
+    return f
+def test(f:dict):
+    xc=int(input("Mitu korda sa tahad mängida? "))
+    for i in range(xc):
+        x=choice(Loe_fail(riigid))
+    return x
